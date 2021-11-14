8e4519e27e558bab2506fb2d231a304692cec009
lml net <46319549+lml-net@users.noreply.github.com>
2021-11-14 15:05:07 +0200

    Update gCtrl.js

--- app/goralut/gCtrl.js
+++ app/goralut/gCtrl.js
@@ -11,7 +11,7 @@ app.controller('gCtrl', function($scope,gSrv) {
     
     $scope.result="";
     $scope.result2="";
-    // $scope.result3="";
+    $scope.imgN="";
     //console.log();
 
     $scope.goral=async function(g,n){
@@ -29,13 +29,18 @@ app.controller('gCtrl', function($scope,gSrv) {
     $scope.goral2=async function(g,n){
         //rnd num 
         rndNum=Math.floor((Math.random()*n)+1);
-       
+     
         await  gSrv.getGoralById2(g,rndNum.toString()).then(function (res2) {
             $scope.res_idname=res2.idname;
             $scope.res_title=res2.title;
             
             $scope.result2=res2.meaning ;
-
+          
+           if(g=="runes25")
+                {
+                $scope.imgN="img/r"+rndNum+".jpg");
+                }
+           
             $scope.$apply();
     })
 } ;
