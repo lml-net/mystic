77d5b33ca276e41212d31bbe5f8ba335a769e336
lml net <46319549+lml-net@users.noreply.github.com>
2021-11-14 13:37:38 +0200

    Update gCtrl.js

--- app/goralut/gCtrl.js
+++ app/goralut/gCtrl.js
@@ -11,7 +11,7 @@ app.controller('gCtrl', function($scope,gSrv) {
     
     $scope.result="";
     $scope.result2="";
-      $scope.result3="";
+    // $scope.result3="";
     //console.log();
 
     $scope.goral=async function(g,n){
@@ -26,32 +26,20 @@ app.controller('gCtrl', function($scope,gSrv) {
     } ;
  
 
-//     $scope.goral2=async function(g,n){
-//         //rnd num 
-//         rndNum=Math.floor((Math.random()*n)+1);
-       
-//         await  gSrv.getGoralById2(g,rndNum.toString()).then(function (res2) {
-//             $scope.res_idname=res2.idname;
-//             $scope.res_title=res2.title;
-            
-//             $scope.result2=res2.meaning ;
-
-//             $scope.$apply();
-//     })
-// } ;
- $scope.goral2=async function(g,n){
+    $scope.goral2=async function(g,n){
         //rnd num 
         rndNum=Math.floor((Math.random()*n)+1);
        
-//         await  gSrv.getGoralById2(g,rndNum.toString()).then(function (res2) {
-//             $scope.res_idname=res2.idname;
-//             $scope.res_title=res2.title;
+        await  gSrv.getGoralById2(g,rndNum.toString()).then(function (res2) {
+            $scope.res_idname=res2.idname;
+            $scope.res_title=res2.title;
             
-            $scope.result3="Runes";//.res2.meaning ;
+            $scope.result2=res2.meaning ;
 
             $scope.$apply();
     })
 } ;
+
 
 
 });
